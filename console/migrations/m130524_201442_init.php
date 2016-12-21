1559c08fa92a42926b9a474bb92c92a840bce79a
Sergey Martusenko <snmartusenko@gmail.com>
2016-12-21 15:39:09 +0200

    migration

--- console/migrations/m130524_201442_init.php
+++ console/migrations/m130524_201442_init.php
@@ -23,6 +23,7 @@ class m130524_201442_init extends Migration
             'status' => $this->smallInteger()->notNull()->defaultValue(10),
             'created_at' => $this->integer()->notNull(),
             'updated_at' => $this->integer()->notNull(),
+            'secret_key' => $this->string()->unique(),
         ], $tableOptions);
     }
 
